395c08aecd9f1d3afdaf9bdd14d961e8060bb85b
rngustj9139 <mnb9139@khu.ac.kr>
2023-02-20 06:30:29 +0900

    Add comments in ValidationItemApiController

--- src/main/java/koo/product/management/system/api/ValidationItemApiController.java
+++ src/main/java/koo/product/management/system/api/ValidationItemApiController.java
@@ -14,13 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/validation/api/items")
 public class ValidationItemApiController {
 
-    @PostMapping("/add")
+    @PostMapping("/add") // Integer (price) 값에 문자를 넣으면 아예 바인딩이 안되서 예외 페이지 발생(컨트롤러 호출 X) (@RequestBody와는 다르게 @ModelAttribute는 일단 바인딩은 되고 컨트롤러가 호출이 된다.)
     public Object addItem(@RequestBody @Validated ItemSaveForm form, BindingResult bindingResult) { // @Valid, @Validated는 HttpMessageConverter(@RequestBody)에서도 적용 가능
         log.info("API 컨트롤러 호출");
 
         if (bindingResult.hasErrors()) {
             log.info("검증 오류 발생 errors={}", bindingResult.getAllErrors());
-
             return bindingResult.getAllErrors();
         }
 
