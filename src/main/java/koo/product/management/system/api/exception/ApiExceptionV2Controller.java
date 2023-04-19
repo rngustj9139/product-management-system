9899068c08b5b22a824fef8754bddc2f565b93ee
rngustj9139 <mnb9139@khu.ac.kr>
2023-04-19 11:25:54 +0900

    ExceptionHandlerExceptionResolver2

--- src/main/java/koo/product/management/system/api/exception/ApiExceptionV2Controller.java
+++ src/main/java/koo/product/management/system/api/exception/ApiExceptionV2Controller.java
@@ -5,19 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
 public class ApiExceptionV2Controller {
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(IllegalArgumentException.class) // IllegalArgumentException 발생시 ErrorResult 객체가 json으로 응답되게 된다.
-    public ErrorResult illegalExHandler(IllegalArgumentException e) {
-        log.error("[exceptionHandler] ex", e);
-
-        return new ErrorResult("BAD", e.getMessage()); // but HTTP 응답 상태 코드가 200으로 된다. 따라서 @ResponseStatus를 붙여야 한다.
-    }
 
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
@@ -41,6 +34,22 @@ public class ApiExceptionV2Controller {
         private String memberId;
         private String name;
 
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class) // IllegalArgumentException 발생시 ErrorResult 객체가 json으로 응답되게 된다.
+    public ErrorResult illegalExHandler(IllegalArgumentException e) {
+        log.error("[exceptionHandler] ex", e);
+
+        return new ErrorResult("BAD", e.getMessage()); // but HTTP 응답 상태 코드가 200으로 된다. 따라서 @ResponseStatus를 붙여야 한다.
+    }
+
+    @ExceptionHandler // 파라미터로 에러 종류를 적을 경우 생략 가능
+    public ResponseEntity<ErrorResult> userExHandler(UserException e) {
+        log.error("[exceptionHandler] ex", e);
+        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
+
+        return new ResponseEntity(errorResult, HttpStatus.BAD_REQUEST);
     }
 
 }
