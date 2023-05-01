6713e874b87c4b281ab824422b4117ea6f2cc5ff
rngustj9139 <mnb9139@khu.ac.kr>
2023-05-01 16:04:40 +0900

    Update ConverterTest

--- src/test/java/koo/product/management/system/springTypeConverter/converter/ConverterTest.java
+++ src/test/java/koo/product/management/system/springTypeConverter/converter/ConverterTest.java
@@ -1,5 +1,6 @@
 package koo.product.management.system.springTypeConverter.converter;
 
+import koo.product.management.system.springTypeConverter.type.IpPort;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -19,6 +20,24 @@ public class ConverterTest {
         String result = converter.convert(10);
 
         Assertions.assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void IpPortToString() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        IpPort source = new IpPort("127.0.0.1", 8080);
+        String result = converter.convert(source);
+
+        Assertions.assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+
+    @Test
+    void StringToIpPort() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        String source = "127.0.0.1:8080";
+        Integer result = converter.convert(source);
+
+        Assertions.assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080)); // @EqualsAndHashCode로 인해 isEqualTo 사용 가능
     }
 
 }
