1a47b8cb86292f0817942dac646ac6a23fab3750
rngustj9139 <mnb9139@khu.ac.kr>
2023-05-02 13:51:46 +0900

    Add Converters in WebConfig

--- src/main/java/koo/product/management/system/WebConfig.java
+++ src/main/java/koo/product/management/system/WebConfig.java
@@ -4,7 +4,12 @@ import koo.product.management.system.api.exception.resolver.MyHandlerExceptionRe
 import koo.product.management.system.api.exception.resolver.UserHandlerExceptionResolver;
 import koo.product.management.system.interceptor.LogInterceptor;
 import koo.product.management.system.interceptor.LoginCheckInterceptor;
+import koo.product.management.system.springTypeConverter.converter.IntegerToStringConverter;
+import koo.product.management.system.springTypeConverter.converter.IpPortToStringConverter;
+import koo.product.management.system.springTypeConverter.converter.StringToIntegerConverter;
+import koo.product.management.system.springTypeConverter.converter.StringToIpPortConverter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -31,6 +36,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) { // api/exception/resolver/MyHandlerExceptionResolver를 위해 등록
         resolvers.add(new MyHandlerExceptionResolver());
         resolvers.add(new UserHandlerExceptionResolver());
+    }
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) { // 컨버터 등록 (스프링은 내부에서 ConversionService 제공한다.)
+        registry.addConverter(new StringToIntegerConverter());
+        registry.addConverter(new IntegerToStringConverter());
+        registry.addConverter(new StringToIpPortConverter());
+        registry.addConverter(new IpPortToStringConverter());
     }
 
 }
--- src/main/java/koo/product/management/system/springTypeConverter/converter/IpPortToStringConverter.java
+++ src/main/java/koo/product/management/system/springTypeConverter/converter/IpPortToStringConverter.java
@@ -10,6 +10,7 @@ import org.springframework.core.convert.converter.Converter;
  *    Integer result = converter.convert(source);
  *    와 같이 컨버터를 직접 불러와 사용하는 것이 어렵기 때문에 개별 컨버전을 모아서 효과적으로 사용할 수 있게하는 ConversionService를 이용해야한다.
  *    ConversionServiceTest 확인하기
+ *    WebConfig에도 컨버전 서비스를 이용해 컨버전을 등록 가능하다(확인하기)
  */
 @Slf4j
 public class IpPortToStringConverter implements Converter<IpPort, String> {
